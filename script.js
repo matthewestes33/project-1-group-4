00d74073aee88f728e0f2caf9169e08566a2d873
Billy Pinkerton <billybattlebones@gmail.com>
2022-04-19 12:55:53 -0700

    updating structure

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-let pasta = tasty 
+let eosData = 
+
+fetch('https://api.waqi.info/feed/phoenix/?token=df975e5218ef33ecbd33be41365e612135104e9b')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
+fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
