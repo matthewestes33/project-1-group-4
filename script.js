78ceaabf7e18fec5b4adc708cfb318f4018ce686
Billy Pinkerton <billybattlebones@gmail.com>
2022-04-22 15:39:42 -0700

    Page working and materialize base

--- script.js
+++ script.js
@@ -1,160 +1,105 @@
-var server = "https://eonet.sci.gsfc.nasa.gov/api/v3";
-// First, show the list of events
-$(document).ready(function () {
-    $.getJSON(server + "/categories/wildfires", {
-        status: "open",
-        limit: 5
-    })
-        .done(function (data) {
-            $.each(data.events, function (key, event) {
-                $("#eventList").append(
-                    "<dt class='event'>" +
-                    "<a href='#' onclick='showLayers(\"" + event.id + "\");'>" +
-                    event.title + "</a></dt>"
-                );
-                if (event.description != null && event.description.length) {
-                    $("#eventList").append(
-                        "<dd>" + event.description + "</dd>"
-                    );
-                }
-            });
-        });
-});
-// Show the available layers for the event category
-function showLayers(eventId) {
-    // hide the events list
-    $("#eventSelect").hide();
-    $("#layerSelect").show();
-    // fetch the single event feed
-    $.getJSON(server + "/events/" + eventId)
-        .done(function (event) {
-            // Get the date and first location of the event.
-            // Events can have multiple locations but we are simplifying here.
-            var location = event.geometry[0];
-            $("#eventTitle").append(": " + event.title + ", " + location.date.substring(0, 10));
-            // Show list of categories and children layers
-            $.each(event.categories, function (key, category) {
-                $("#layerList").append(
-                    "<dt>" + category.title + "</dt> "
-                );
-                // Get the applicable layers for the specific event category.
-                // Only include WMTS_1_0_0 layers for now, will add WMS example later.
-                $.getJSON(server + "/layers/" + category.id)
-                    .done(function (data) {
-                        var layers = data['categories'][0]['layers'];
-                        $.each(layers, function (key, layer) {
-                            if (layer.serviceTypeId == "WMTS_1_0_0") {
-                                $("#layerList").append(
-                                    "<dd>" +
-                                    "<a href='#' onclick='showMap(\"" + encodeURIComponent(JSON.stringify(layer)) + "\", \"" + encodeURIComponent(JSON.stringify(location)) + "\");'>" +
-                                    layer.name + "</a></dd> "
-                                );
-                            }
-                        });
-                    });
-            });
-        });
-}
-function showMap(encodedLayer, encodedLocation) {
-    var layer = JSON.parse(decodeURIComponent(encodedLayer));
-    var location = JSON.parse(decodeURIComponent(encodedLocation));
-    var center = getCenter(location);
-    // quick and dirty way to extract day string from full ISO datetime
-    var mapTime = new Date(location.date).toJSON().substring(0, 10);
-    displayMap(layer.serviceUrl, layer.name,
-        center, mapTime,
-        layer.parameters[0].FORMAT, layer.parameters[0].TILEMATRIXSET);
-}
-function getCenter(geojson) {
-    if (geojson.type == "Point") {
-        return geojson.coordinates;
-    } else if (geojson.type == "Polygon") {
-        /*
-        For this test we are going to compute the center point of the bounding box
-         that encloses the geoJson Polygon.
-         Since the Polygon specification consists of an outer ring and then inner holes,
-         we will only compute the center of the first (outer) LinearRing in the Polygon.
-         Convert these coordinates to 0-360 to make it easier
-         */
-        // use the first point of the first LinearRing as the default for calculations
-        var ullat = geojson.coordinates[0][0][1] + 90;
-        var ullon = geojson.coordinates[0][0][0] + 180;
-        var lrlat = geojson.coordinates[0][0][1] + 90;
-        var lrlon = geojson.coordinates[0][0][0] + 180;
-        for (i = 0; i < geojson.coordinates[0].length; i++) {
-            // longitudes
-            if (geojson.coordinates[0][i][0] + 180 > ullon) {
-                ullon = geojson.coordinates[0][i][0] + 180;
-            }
-            if (geojson.coordinates[0][i][0] + 180 < lrlon) {
-                lrlon = geojson.coordinates[0][i][0] + 180;
-            }
-            // latitudes
-            if (geojson.coordinates[0][i][1] + 90 > ullat) {
-                ullat = geojson.coordinates[0][i][1] + 90;
-            }
-            if (geojson.coordinates[0][i][1] + 90 < lrlat) {
-                lrlat = geojson.coordinates[0][i][1] + 90;
-            }
-        }
-        centerX = (ullon + ((lrlon - ullon) / 2)) - 180;
-        centerY = (lrlat + ((ullat - lrlat) / 2)) - 90;
-        return [centerX, centerY];
+
+
+//Variables to pull the data from EONET for the current tracked wildfires
+let dataDisplay1 = document.getElementById('data-display1');
+let dataDisplay2 = document.getElementById('data-display2');
+let dataDisplay3 = document.getElementById('data-display3');
+let dataDisplay4 = document.getElementById('data-display4');
+let dataDisplay5 = document.getElementById('data-display5');
+//Variables to join the AQI data to the document under the EONET data.
+let aqiDisplay1 = document.getElementById('aqi-display1');
+let aqiDisplay2 = document.getElementById('aqi-display2');
+let aqiDisplay3 = document.getElementById('aqi-display3');
+let aqiDisplay4 = document.getElementById('aqi-display4');
+let aqiDisplay5 = document.getElementById('aqi-display5');
+
+//url call for EONET
+var eosUrl = 'https://eonet.gsfc.nasa.gov/api/v3/categories/wildfires'
+//function to pull the response from EONet and combine it with the addAqi function to display the event and the nearby aqi then append it to the document.
+async function getEos1() {
+    var response = await fetch(eosUrl)
+    var data = await response.json();
+    var lat = data.events[0].geometry[0].coordinates[0]
+    var lon = data.events[0].geometry[0].coordinates[1]
+    for (i = 0; i < 1; i++) {
+        console.log(data.events[i].geometry[0].coordinates)
+        console.log(data.events[i].title)
+        dataDisplay1.append(data.events[i].title);
+        aqiDisplay1.append(" AQI: " + await addAqi(lon, lat));
     }
+    addAqi(lon, lat);
 }
-function displayMap(serviceUrl, layerName, center, dateStr, format, matrixSet) {
-    // call empty() to make sure another map doesn't already exist there
-    $("#map").empty();
-    var map = new ol.Map({
-        view: new ol.View({
-            maxResolution: 0.5625,
-            projection: ol.proj.get("EPSG:4326"),
-            extent: [-180, -90, 180, 90],
-            center: center,
-            zoom: 3,
-            maxZoom: 8
-        }),
-        target: "map",
-        renderer: ["canvas", "dom"]
-    });
-    /*
-     This determination of resolutions is based solely on the capabilities
-     of the NASA GIBS WMTS as it is currently the only WMTS server represented
-     in EONET. More information about GIBS: https://go.nasa.gov/1GTDj3V
-     */
-    var source = new ol.source.WMTS({
-        url: serviceUrl + "?time=" + dateStr,
-        layer: layerName,
-        format: format,
-        matrixSet: matrixSet,
-        tileGrid: new ol.tilegrid.WMTS({
-            origin: [-180, 90],
-            resolutions: [
-                0.5625,
-                0.28125,
-                0.140625,
-                0.0703125,
-                0.03515625,
-                0.017578125,
-                0.0087890625,
-                0.00439453125,
-                0.002197265625
-            ],
-            matrixIds: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-            tileSize: 512
-        })
-    });
-    var layer = new ol.layer.Tile({
-        source: source
-    });
-    map.addLayer(layer);
+//this is a repeat of the getEos function but built to run and display to a new <p> tag, 
+async function getEos2() {
+    var response = await fetch(eosUrl)
+    var data = await response.json();
+    var lat = data.events[1].geometry[0].coordinates[0]
+    var lon = data.events[1].geometry[0].coordinates[1]
+    for (i = 1; i < 2; i++) {
+        console.log(data.events[i].geometry[0].coordinates)
+        console.log(data.events[i].title)
+        dataDisplay2.append(data.events[i].title);
+        aqiDisplay2.append(" AQI: " + await addAqi(lon, lat));
+    }
+    addAqi(lon, lat);
 }
 
+async function getEos3() {
+    var response = await fetch(eosUrl)
+    var data = await response.json();
+    var lat = data.events[2].geometry[0].coordinates[0]
+    var lon = data.events[2].geometry[0].coordinates[1]
+    for (i = 2; i < 3; i++) {
+        console.log(data.events[i].geometry[0].coordinates)
+        console.log(data.events[i].title)
+        dataDisplay3.append(data.events[i].title);
+        aqiDisplay3.append(" AQI: " + await addAqi(lon, lat));
+    }
+    addAqi(lon, lat);
+}
+async function getEos4() {
+    var response = await fetch(eosUrl)
+    var data = await response.json();
+    var lat = data.events[3].geometry[0].coordinates[0]
+    var lon = data.events[3].geometry[0].coordinates[1]
+    for (i = 3; i < 4; i++) {
+        console.log(data.events[i].geometry[0].coordinates)
+        console.log(data.events[i].title)
+        dataDisplay4.append(data.events[i].title);
+        aqiDisplay4.append(" AQI: " + await addAqi(lon, lat));
+    }
+    addAqi(lon, lat);
+}
+async function getEos5() {
+    var response = await fetch(eosUrl)
+    var data = await response.json();
+    var lat = data.events[4].geometry[0].coordinates[0]
+    var lon = data.events[4].geometry[0].coordinates[1]
+    for (i = 4; i < 5; i++) {
+        console.log(data.events[i].geometry[0].coordinates)
+        console.log(data.events[i].title)
+        dataDisplay5.append(data.events[i].title);
+        aqiDisplay5.append(" AQI: " + await addAqi(lon, lat));
+    }
+    addAqi(lon, lat);
+}
+//this is a function to grab the data from air quality api from open weather and attache the coordinates given by the EONET api
+async function addAqi(lat, lon) {
+    let aqiUrl = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=' + lat + '&lon=' + lon + '&appid=461b72ac82582b3246592edff52f2471';
+    let response = await fetch(aqiUrl)
+    let dataAqi = await response.json();
+        console.log(dataAqi.list)
+return dataAqi.list[0].main.aqi
+    }
+//calls the functions to display the data on the document.
+getEos1();
+getEos2();
+getEos3();
+getEos4();
+getEos5();
 
-/*aqiFeed({city:"beijing",callback:function(aqi){console.log(aqi);}});*/
 
-/*aqiFeed({city:"beijing", lang:"pl", callback:function(aqi){	$("#my-container").html(aqi.details)}});*/
 
-(function(w,d,t,f){w[f]=w[f]||function(c,k,n){s=w[f],k=s['k']=(s['k']||(k?('&k='+k):''));s['c']=c=(c instanceof Array)?c:[c];s['n']=n=n||0;L=d.createElement(t),e=d.getElementsByTagName(t)[0];L.async=1;L.src='//feed.aqicn.org/feed/'+(c[n].city)+'/'+(c[n].lang||'')+'/feed.v1.js?n='+n+k;e.parentNode.insertBefore(L,e);}})(window,document,'script','_aqiFeed');
 
-_aqiFeed({container:"city-aqi-container",city:"phoenix"});
+
+
